8f0b1539ecf9c0a97b4dba597ddeeb54a3369890
Hyperling <me@hyperling.com>
2024-07-20 12:37:54 -0700

    Add class for easy control of expense records.

---
+++ app/src/main/java/com/hyperling/expensetracker/Expense.kt
@@ -0,0 +1,23 @@
+package com.hyperling.expensetracker
+
+/*class Expense {
+    var name: String = ""
+    var cost: Double = 0.0
+    var freq: Char = '*'
+    var note: String = ""
+
+    /*public Expense (val name: String, val cost: Double, val freq: Char, val note: String) {
+        this.name = name
+        this.name = name
+        this.name = name
+        this.name = name
+    }*/
+}*/
+
+class Expense (
+    var name: String = "",
+    var cost: Double = 0.0,
+    var freq: Char = '*',
+    var note: String = "",
+) {
+}
